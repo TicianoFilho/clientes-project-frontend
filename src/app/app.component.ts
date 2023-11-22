2eac5455627b7a5c8dbdb191649d0f517640c686
ticiano.filho <ticianofilho@gmail.com>
2023-11-22 18:00:33 -0300

    adding navbar event open and close - still implementing...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import jQuery from 'jQuery'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'clientes-project-front';
+
+  ngAfterViewInit(): void {
+    (function($) {
+      "use strict";
+
+      // Add active state to sidebar nav links
+      var path = window.location.href;
+      $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+        // Use 'this' as HTMLElement
+        if ((this as HTMLAnchorElement).href === path) {
+          $(this).addClass("active");
+        }
+      });
+
+      // Toggle the side navigation
+      $("#sidebarToggle").on("click", function(e: Event) {
+        e.preventDefault();
+        $("body").toggleClass("sb-sidenav-toggled");
+      });
+    })(jQuery);
+  }
 }
