3a63236c2fb68273773265f30d0ca10781d00ae1
ticiano.filho <ticianofilho@gmail.com>
2023-11-23 11:12:39 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {  } from './home'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component:  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TemplateModule } from './template/template.module';
+import { ClientesModule } from './clientes/clientes.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { TemplateModule } from './template/template.module';
     BrowserModule,
     AppRoutingModule,
     TemplateModule,
+    ClientesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
