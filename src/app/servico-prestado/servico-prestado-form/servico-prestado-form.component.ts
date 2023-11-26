8a511dd1d72953c3fe3dd7e84b4a674db34555b1
TicianoFilho <ticianofilho@gmail.com>
2023-11-26 12:11:34 -0300

    add select loading all clientes

--- src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientesService } from 'src/app/clientes.service';
+import { Cliente } from 'src/app/clientes/Clientes';
 
 @Component({
   selector: 'app-servico-prestado-form',
   templateUrl: './servico-prestado-form.component.html',
   styleUrls: ['./servico-prestado-form.component.css']
 })
-export class ServicoPrestadoFormComponent {
+export class ServicoPrestadoFormComponent implements OnInit {
+
+  clientes: Cliente[];
+
+  constructor(private clienteService: ClientesService) {
+    this.clienteService = clienteService;
+  }
+
+  ngOnInit(): void {
+    this.LoadClientes();
+  }
+
+  onSubmit() {
+    console.log(this.clientes);
+  }
+
+  LoadClientes() {
+    this.clienteService.getAll().subscribe(response => this.clientes = response);
+  }
 
 }
