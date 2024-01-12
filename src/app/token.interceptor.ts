ec2155b4dae89b5768927ed04b25feb5620fcb2d
ticiano.filho <ticianofilho@gmail.com>
2024-01-11 22:53:46 -0300

    implemented http intercept for Bearer token to inject token in the header

--- src/app/token.interceptor.ts
+++ src/app/token.interceptor.ts
@@ -13,6 +13,21 @@ export class TokenInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+
+    // get the token in the localStorage
+    const tokenString = localStorage.getItem('access_token');
+
+    // add Authentication header
+    if (tokenString) {
+      const token = JSON.parse(tokenString);
+      const jwt = token.access_token;
+      request = request.clone({
+        setHeaders : {
+          Authorization : `Bearer ${jwt}`,
+        }
+      })
+    }
+
     return next.handle(request);
   }
 }
